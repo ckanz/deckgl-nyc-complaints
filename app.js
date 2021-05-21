fadc51c311023a3f08bbeeebc4d742de5c94143d
Clemens Anzmann <clemens.anzmann@gmail.com>
2021-05-21 10:28:27 +0100

    migrate project to latest deckgl version

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import MapGL from 'react-map-gl';
-import tokens from './creds.js' // gitignored
-import DeckGLOverlay from './deckgl-overlay.js';
-import {csv as requestCsv} from 'd3-request';
-const MAPBOX_TOKEN = tokens.mapboxToken;
+import React from 'react'
+import { render } from 'react-dom'
+import { StaticMap } from 'react-map-gl'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
+import DeckGL from '@deck.gl/react'
 
-class Root extends Component {
+// Source data CSV
+const DATA_URL =
+  'https://raw.githubusercontent.com/ckanz/deckgl-nyc-complaints/master/data/nyc-complaint-data.csv'; // eslint-disable-line
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        ...DeckGLOverlay.defaultViewport,
-        width: 500,
-        height: 500
-      },
-      data: null
-    };
-
-    requestCsv('./data/nyc-complaint-data.csv', (error, response) => {
-      if (!error) {
-        const data = response.map(d => ([Number(d.lng), Number(d.lat)]));
-        this.setState({data});
-      }
-    });
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this._resize.bind(this));
-    this._resize();
-  }
-
-  _resize() {
-    this._onChangeViewport({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-  }
-
-  _onChangeViewport(viewport) {
-    this.setState({
-      viewport: {...this.state.viewport, ...viewport}
-    });
-  }
-
-  render() {
-    const {viewport, data} = this.state;
-
-    return (
-      <MapGL
-        {...viewport}
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        perspectiveEnabled={true}
-        onChangeViewport={this._onChangeViewport.bind(this)}
-        mapboxApiAccessToken={MAPBOX_TOKEN}>
-        <DeckGLOverlay
-          viewport={viewport}
-          data={data || []}
-        />
-      </MapGL>
-    );
-  }
+const INITIAL_VIEW_STATE = {
+  longitude: -74.0,
+  latitude: 40.68,
+  zoom: 10.5,
+  minZoom: 5,
+  maxZoom: 15,
+  pitch: 50,
+  bearing: 0
 }
 
-render(<Root />, document.body.appendChild(document.createElement('div')));
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json'
+
+/* eslint-disable react/no-deprecated */
+export default function App ({
+  data,
+  mapStyle = MAP_STYLE,
+  radius = 25,
+  upperPercentile = 100,
+  coverage = 0.8
+}) {
+  const layers = [
+    new HexagonLayer({
+      id: 'heatmap',
+      coverage,
+      data,
+      elevationRange: [0, 200],
+      elevationScale: data && data.length ? 50 : 0,
+      extruded: true,
+      getPosition: d => d,
+      radius,
+      upperPercentile,
+      transitions: {
+        elevationScale: 200
+      }
+    })
+  ]
+
+  return (
+    <DeckGL
+      layers={layers}
+      initialViewState={INITIAL_VIEW_STATE}
+      controller
+    >
+      <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing />
+    </DeckGL>
+  )
+}
+
+export function renderToDOM (container) {
+  render(<App />, container)
+
+  require('d3-request').csv(DATA_URL, (error, response) => {
+    if (!error) {
+      const data = response.map(d => [Number(d.lng), Number(d.lat)])
+      render(<App data={data} />, container)
+    }
+  })
+}
